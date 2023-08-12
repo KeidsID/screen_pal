ef693da59fbd37ba2d6f69fee3ef7810c2f8eedd
KeidsID <keidscode@gmail.com>
2023-08-12 20:06:48 +0800

    refactor(core): refactor Movie entity
    Also add MIT LICENSE, and update MainApp to preview theme.

--- lib/core/entities/movie.dart
+++ lib/core/entities/movie.dart
@@ -1,32 +1,24 @@
 class Movie {
   final bool adult;
-  final String backdropPath;
-  final List<String> genres;
   final int id;
-  final String originalLanguage;
   final String originalTitle;
   final String overview;
   final double popularity;
   final String posterPath;
-  final String releaseDate;
+  final DateTime releaseDate;
   final String title;
-  final bool video;
   final double voteAverage;
   final int voteCount;
 
-  Movie({
+  const Movie({
     required this.adult,
-    required this.backdropPath,
-    required this.genres,
     required this.id,
-    required this.originalLanguage,
     required this.originalTitle,
     required this.overview,
     required this.popularity,
     required this.posterPath,
     required this.releaseDate,
     required this.title,
-    required this.video,
     required this.voteAverage,
     required this.voteCount,
   });
