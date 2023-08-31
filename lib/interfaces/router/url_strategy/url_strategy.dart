5b6001a95cdfcfbc0c44359d2046b2c3181835fc
KeidsID <keidscode@gmail.com>
2023-08-31 14:53:51 +0800

    fix(coverage): add missing coverage ignore

--- lib/interfaces/router/url_strategy/url_strategy.dart
+++ lib/interfaces/router/url_strategy/url_strategy.dart
@@ -1,1 +1,3 @@
+// coverage:ignore-file
+
 export 'other.dart' if (dart.library.html) 'web.dart';
--- lib/interfaces/themes/espresso_and_crema_theme.dart
+++ lib/interfaces/themes/espresso_and_crema_theme.dart
@@ -1,3 +1,5 @@
+// coverage:ignore-file
+
 import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:google_fonts/google_fonts.dart';
