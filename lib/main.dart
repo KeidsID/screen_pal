5d11400f62a7d4080070d3655dbebaad9be21ed3
KeidsID <keidscode@gmail.com>
2023-12-14 01:08:10 +0800

    feat(splash): splash removed when needed resource are loaded.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:screen_pal/interfaces/providers/remote/extras/languages_provider.dart';
 
 import 'common/constants.dart';
 import 'common/envs/env.dart';
 import 'infrastructures/container/container.dart' as container;
 import 'interfaces/providers/cache/app_theme_mode_provider.dart';
+import 'interfaces/providers/remote/extras/genres_providers.dart';
 import 'interfaces/router/router.dart';
 import 'interfaces/router/url_strategy/url_strategy.dart';
 import 'interfaces/themes/app_themes.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   await container.init();
 
@@ -20,11 +24,30 @@ void main() async {
   runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends ConsumerWidget {
+class MainApp extends ConsumerStatefulWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends ConsumerState<MainApp> {
+  bool _isSplashRemoved = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final movieGenres = ref.watch(movieGenresProvider);
+    final tvShowGenres = ref.watch(tvShowGenresProvider);
+    final languages = ref.watch(languagesProvider);
+
+    if (!_isSplashRemoved &&
+        movieGenres.isNotEmpty &&
+        tvShowGenres.isNotEmpty &&
+        languages.isNotEmpty) {
+      FlutterNativeSplash.remove();
+      setState(() => _isSplashRemoved = true);
+    }
+
     return MaterialApp.router(
       routerConfig: router,
       debugShowCheckedModeBanner: false,
