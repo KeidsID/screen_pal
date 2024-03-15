19a5bb6df1272a610aa6dcde641845a24a8559a2
KeidsID <keidscode@gmail.com>
2024-03-15 11:35:39 +0800

    refactor: format files

--- packages/screen_pal_core/lib/src/use_cases/remote/movies/get_now_playing_movies_case.dart
+++ packages/screen_pal_core/lib/src/use_cases/remote/movies/get_now_playing_movies_case.dart
@@ -1,5 +1,6 @@
 import 'package:screen_pal_core/src/entities.dart';
 import 'package:screen_pal_core/src/services.dart';
+
 final class GetNowPlayingMoviesCase {
   const GetNowPlayingMoviesCase({required MoviesRepo moviesRepo})
       : _moviesRepo = moviesRepo;
