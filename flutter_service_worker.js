57e9b0f644321851c633c72a5c4b31a018435abc
KeidsID <keidscode@gmail.com>
2024-03-15 16:08:43 +0000

    web-preview build from PR https://github.com/KeidsID/screen_pal/pull/12

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {"splash/img/light-4x.png": "99167859e575099ed7a0fc56762d4ff7"
 "splash/img/dark-1x.png": "6b61145fdbae1c38741cfcaf51f517ba",
 "splash/img/dark-3x.png": "245273b7f6f14fff60dc7006573388c1",
 "splash/img/light-1x.png": "6b61145fdbae1c38741cfcaf51f517ba",
-"index.html": "a7b7a36f97200e1a55047766329f8f06",
-"/": "a7b7a36f97200e1a55047766329f8f06",
+"index.html": "d3f42a3f0ed0082052580bcecf3c5174",
+"/": "d3f42a3f0ed0082052580bcecf3c5174",
 "canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
 "canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
 "canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
@@ -58,7 +58,7 @@ const RESOURCES = {"splash/img/light-4x.png": "99167859e575099ed7a0fc56762d4ff7"
 "icons/Icon-192.png": "72fc2a029a9c1dcc94e837a354219ecf",
 "manifest.json": "93b435a70934830ff4967caad2378fd0",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"main.dart.js": "55bcafe238d545771cbb89296507bf7a"};
+"main.dart.js": "3c343ac7579f8daff25ddc651da8c415"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
