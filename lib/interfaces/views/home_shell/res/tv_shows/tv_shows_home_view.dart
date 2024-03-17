e97d41ed67396b280740824cae22f217e2d41d36
KeidsID <keidscode@gmail.com>
2024-03-17 15:47:52 +0800

    fix(providers): refactor to pass "custom_lint" analyze.
    - remove "interfaces" tests.

--- lib/interfaces/views/home_shell/res/tv_shows/tv_shows_home_view.dart
+++ lib/interfaces/views/home_shell/res/tv_shows/tv_shows_home_view.dart
@@ -1,7 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:screen_pal/interfaces/providers/remote/tv_shows/tv_show_list_providers.dart';
 import 'package:screen_pal/interfaces/widgets.dart';
+import 'package:screen_pal_core/screen_pal_core.dart';
 
 class TvShowsHomeView extends StatelessWidget {
   const TvShowsHomeView({super.key});
@@ -17,23 +20,62 @@ class TvShowsHomeView extends StatelessWidget {
             children: [
               ProductsCarouselSection(popularTvShowsProvider),
               const SizedBox(height: 16.0),
-              ProductListSection(
+              _TvShowsSection(
                 title: 'Airing Today',
-                productsProvider: airingTodayTvShowsProvider,
+                provider: airingTodayTvShowsProvider,
               ),
               const SizedBox(height: 16.0),
-              ProductListSection(
+              _TvShowsSection(
                 title: 'On Air',
-                productsProvider: onAirTvShowsProvider,
+                provider: onAirTvShowsProvider,
               ),
               const SizedBox(height: 16.0),
-              ProductListSection(
+              _TvShowsSection(
                 title: 'Top Rated',
-                productsProvider: topRatedTvShowsProvider,
+                provider: topRatedTvShowsProvider,
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _TvShowsSection extends ConsumerWidget {
+  const _TvShowsSection({
+    required this.title,
+    required this.provider,
+  });
+
+  final String title;
+  final FutureProvider<List<TvShow>> provider;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tvShowsAsyncValue = ref.watch(provider);
+
+    final products = tvShowsAsyncValue.asData?.value ?? [];
+
+    return ProductListSection(
+      title: title,
+      products: products,
+      alt: tvShowsAsyncValue.whenOrNull(
+        skipLoadingOnRefresh: false,
+        loading: () => const SizedCircularProgressIndicator.expand(),
+        error: (err, trace) {
+          final action = ElevatedButton.icon(
+            onPressed: () => ref.refresh(provider),
+            icon: const Icon(Icons.refresh_outlined),
+            label: const Text('Refresh'),
+          );
+
+          if (err is DioException) {
+            return SizedDioExceptionWidget.expand(err, action: action);
+          }
+
+          return SizedExceptionWidget.expand(err, trace: trace, action: action);
+        },
       ),
     );
   }
--- lib/interfaces/widgets/others/products/product_list_section.dart
+++ lib/interfaces/widgets/others/products/product_list_section.dart
@@ -1,28 +1,14 @@
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fl_utilities/fl_utilities.dart';
+import 'package:flutter/material.dart';
 
 import 'package:screen_pal/common/constants.dart';
 import 'package:screen_pal/interfaces/widgets.dart';
 import 'package:screen_pal_core/screen_pal_core.dart';
 
-/// {@template screen_pal.interfaces.widgets.products.ProductListSection}
-/// Common product list view section on the app.
-///
-/// - [autoDisposeProductsProvider], primary product provider. Other providers
-///   will be ignored if this is not null.
-/// - [productsProvider], secondary product provider.
-/// - [products], tertiary/fallback product provider.
-/// {@endtemplate}
 class ProductListSection extends StatelessWidget {
-  /// {@macro screen_pal.interfaces.widgets.products.ProductListSection}
   const ProductListSection({
     super.key,
     required this.title,
-    this.autoDisposeProductsProvider,
-    this.productsProvider,
     this.products = const <Product>[],
     this.alt,
     this.padding = kHorizPadding,
@@ -31,73 +17,24 @@ class ProductListSection extends StatelessWidget {
   /// Section header.
   final String title;
 
-  /// Primary product provider. Other providers will be ignored if this
-  /// is not null.
-  final AutoDisposeFutureProvider<List<Product>>? autoDisposeProductsProvider;
-
-  /// Secondary product provider. If this also null, then [products] value will
-  /// be used instead.
-  final FutureProvider<List<Product>>? productsProvider;
-
-  /// Tertiary/Fallback product provider. If this an empty [List], then section
-  /// will not be visible ([SizedBox.shrink] will be rendered instead).
   final List<Product> products;
 
-  /// Alternative information of the section that will be displayed if the
-  /// [autoDisposeProductsProvider] or [productsProvider] provide an empty
-  /// [List].
-  ///
-  /// Note that [products] will ignore this.
-  final String? alt;
+  /// Alternative widget to display if [products] is empty.
+  /// 
+  /// Default widget is centered "No data" text.
+  final Widget? alt;
 
   final EdgeInsets padding;
 
   @override
   Widget build(BuildContext context) {
-    if (!_checkProducts()) return const SizedBox.shrink();
-
-    if (autoDisposeProductsProvider == null && productsProvider == null) {
-      return _layout(child: ProductHorizListView(products, padding: padding));
+    if (products.isEmpty) {
+      return _layout(child: alt ?? Center(
+        child: Text('No data', style: context.textTheme.titleMedium),
+      ));
     }
 
-    return _layout(child: Consumer(builder: (_, ref, __) {
-      final watchedProv = autoDisposeProductsProvider ?? productsProvider!;
-      final prov = ref.watch(watchedProv);
-
-      return prov.when(
-        skipLoadingOnRefresh: false,
-        loading: () => const SizedCircularProgressIndicator.expand(),
-
-        //
-        error: (e, trace) {
-          final action = ElevatedButton.icon(
-            onPressed: () => ref.refresh(watchedProv),
-            icon: const Icon(Icons.refresh_outlined),
-            label: const Text('Refresh'),
-          );
-
-          if (e is DioException) {
-            return SizedDioExceptionWidget.expand(e, action: action);
-          }
-
-          return SizedExceptionWidget.expand(e, trace: trace, action: action);
-        },
-
-        //
-        data: (products) {
-          if (products.isEmpty) {
-            return Center(
-              child: Text(
-                alt ?? 'No data',
-                style: context.textTheme.titleMedium,
-              ),
-            );
-          }
-
-          return ProductHorizListView(products, padding: padding);
-        },
-      );
-    }));
+    return _layout(child: ProductHorizListView(products, padding: padding));
   }
 
   Widget _layout({Widget? child}) {
@@ -113,18 +50,5 @@ class ProductListSection extends StatelessWidget {
         SizedBox(height: 240.0, child: child),
       ],
     );
-  }
-
-  bool _checkProducts() {
-    return (autoDisposeProductsProvider != null ||
-        productsProvider != null ||
-        products.isNotEmpty);
-  }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-
-    properties.add(kIsVisibleFlagProperty(_checkProducts()));
   }
 }
